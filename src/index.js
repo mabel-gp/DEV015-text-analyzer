4c650e9b37aee64d09e1807f793c5885f8bd54b7
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-05-17 23:38:49 -0500

    Agregar elementos en HTML y modificar index.js
    Incorporé algunos elementos HTML y cambié index.js para aprender sobre el evento input.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+
+//Esto es una prueba para verificar si funciona addEventListener cuando se vincula con querySelector
+/* let prueba = document.querySelector('[name="user-input"]');
+prueba.addEventListener("click", pruebaDos);
+
+function pruebaDos(){
+    console.log('hola');
+}; */
+
+
+//Esto es una prueba para verificar si funciona el evento "input", me ayudé de console.log
+let palabras = document.querySelector('[name="user-input"]');
+palabras.addEventListener("input",contarPalabras);
+
+function contarPalabras(){
+    console.log('hola');
+};
